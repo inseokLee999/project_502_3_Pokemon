f53d97ebd15980ddc14f635cf21712395e860359
inseokLee999 <syt06137@naver.com>
2024-06-24 17:45:22 +0900

    기본 세팅

---
+++ src/main/java/java/org/choongang/global/router/HandlerMapping.java
@@ -0,0 +1,10 @@
+package java.org.choongang.global.router;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
+
+public interface HandlerMapping {
+    List<Object> search(HttpServletRequest request);
+
+}
---
+++ src/main/java/webapp/WEB-INF/templates/main/index.jsp
@@ -0,0 +1,6 @@
+<%@page contentType="text/html; charset=UTF-8" %>
+<%@taglib prefix="layout" tagdir="/WEB-INF/tags/layouts" %>
+<layout:main>
+    <h1>메인 페이지</h1>
+
+</layout:main>
