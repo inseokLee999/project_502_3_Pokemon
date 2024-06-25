d6957c2cb29db37d1005486844648cd93890ba9b
youngsan01 <intokys@gmail.com>
2024-06-25 20:27:26 +0900

    회원가입 faker test

--- src/test/java/org/choongang/member/controllers/RequestJoin.java
+++ src/test/java/org/choongang/member/controllers/RequestJoin.java
@@ -1,8 +1,10 @@
 package org.choongang.member.controllers;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class RequestJoin {
     private String id;
     private String password;
--- src/test/java/org/choongang/member/services/JoinService.java
+++ src/test/java/org/choongang/member/services/JoinService.java
@@ -1,10 +1,16 @@
 package org.choongang.member.services;
 
 import org.choongang.member.controllers.RequestJoin;
+import org.choongang.member.mapper.MemberMapper;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.conversions.Validator;
 
 // 회원 가입 기능
 public class JoinService {
+    public JoinService() {
+
+    }
     public void process(RequestJoin form) {
+
 
     }
 }
