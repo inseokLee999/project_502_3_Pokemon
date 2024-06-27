625ca98364a8c6f95709bf1c0d6eddda65a4babe
irine3355 <irine3355@gmail.com>
2024-06-27 09:20:05 +0900

    게임 초기 작업

---
+++ src/test/java/org/choongang/game/services/GameInfoService.java
@@ -0,0 +1,44 @@
+package org.choongang.game.services;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import jakarta.servlet.http.HttpServletResponse;
+import org.choongang.global.services.ApiRequestService;
+import org.choongang.global.services.ObjectMapperService;
+import org.choongang.pokemon.entities.api.Pokemon;
+import org.choongang.pokemon.services.PokemonSaveService;
+
+import java.net.http.HttpResponse;
+import java.util.Optional;
+
+public class GameInfoService {
+
+    private final ApiRequestService service;
+    private final ObjectMapperService om;
+    private final PokemonSaveService saveService;
+
+
+    /**
+     * 조회 번호로 포켓몬 상세 정보 조회 업데이트
+     *
+     * @param seq
+     * @return
+     */
+    public Optional<Pokemon> update(long seq) {
+
+        String url = apiUrl + "/pokemon/" + seq;
+
+        Pokemon pokemon = null;
+        HttpResponse<String> response = service.request(url);
+
+        if (response.statusCode() == HttpServletResponse.SC_OK) {
+            try {
+                pokemon = om.readValue(response.body(), Pokemon.class);
+                pokemon.setRawData(response.body());
+
+                saveService.save(pokemon);
+
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        }
+}
---
+++ src/test/java/org/choongang/game/services/GameService.java
@@ -0,0 +1,61 @@
+package org.choongang.game.services;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.Getter;
+import org.choongang.game.mappers.GameMapper;
+import org.choongang.global.config.annotations.Service;
+import org.choongang.pokemon.entities.PokemonDetail;
+import org.choongang.pokemon.entities.api.Pokemon;
+import org.choongang.pokemon.mappers.PokemonMapper;
+
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Map;
+
+
+@Getter
+@Service
+public class GameService {
+
+    /**
+     * 1. 게임시작 웹에서get
+     * 2. DB에서 개인소유 포켓못 불러옴
+     * 3. 대결할 상대 포켓몬 랜덤 선택후 대결 계산
+     * 4. 승자 확인 후 디비에 저장
+     * 5. 웹에post
+     */
+
+    /* km 추가  S */
+
+    Member member = gameMapper.get(form.getUserId());
+
+
+
+    // 게임 루프
+
+
+
+
+            /* km 추가  E */
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
