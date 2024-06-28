1318da188f76c1fe3a73c5259f7ec56e5e992e1e
inseokLee999 <syt06137@naver.com>
2024-06-28 15:35:00 +0900

    랜덤 포켓몬 보드컨트롤러

--- src/main/java/org/choongang/pokemon/controllers/PokemonController.java
+++ src/main/java/org/choongang/pokemon/controllers/PokemonController.java
@@ -35,7 +35,7 @@ public class PokemonController {
 
         return "pokemon/index";
     }
-    @GetMapping("/pokemon/{seq}")
+    @GetMapping("/view/{seq}")
     public String view(@PathVariable("seq") long seq){
         commonProcess();
         request.setAttribute("addCss",List.of("pokemon/view"));
