9e56f1b901df2036583193c649eae97ea33399fc
yoojung275 <yoojung275@gmail.com>
2024-06-28 15:15:58 +0900

    로그인 아웃 기능 구현, (사용자)님 로그인 중, 마이페이지, 로그아웃 버튼 생성

--- src/main/webapp/static/js/pokemon/board.js
+++ src/main/webapp/static/js/pokemon/board.js
@@ -1,7 +1,16 @@
-let ul = document.querySelector('.pokemon-list')
-ul.addEventListener('mousemove', function(e) {
-    let x = e.offsetX;
-    var y = e.offsetY
-    console.log(x, y);
-    container.style = 'transform : rotateY(-20deg)'
-})
+document.addEventListener("DOMContentLoaded", function() {
+    var containers = document.querySelectorAll('.pokemon-data');
+    containers.forEach(function(container) {
+        container.addEventListener('mousemove', function(e) {
+            let x = e.offsetX;
+            var y = e.offsetY;
+            var rotateX = 4/30 * y-20;
+            var rotateY = -1/5 * x+20;
+            container.style.transform = `perspective(350px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+        });
+
+        container.addEventListener('mouseleave', function() {
+            container.style.transform = 'rotateY(0deg)';
+        });
+    })
+});
