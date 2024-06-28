5a178545b827c8f1f4183969cab4a8d448c8d797
irine3355 <irine3355@gmail.com>
2024-06-28 21:21:39 +0900

    디자인-메인 수정

---
+++ src/main/webapp/static/js/main/index.js
@@ -0,0 +1,15 @@
+// main.js 파일 예제
+document.addEventListener('DOMContentLoaded', function() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        autoplay: {
+            delay: 2500,  // 2.5초마다 슬라이드 변경
+            disableOnInteraction: false,  // 사용자가 슬라이드 클릭해도 자동 재생 계속
+        },
+    });
+});
