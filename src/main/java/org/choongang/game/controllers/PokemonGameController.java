cdeee527c112054afffa1b9474091be956c88fc7
minsukim002 <elancla14@gmail.com>
2024-07-02 21:03:05 +0900

    ...

--- src/main/java/org/choongang/game/controllers/PokemonGameController.java
+++ src/main/java/org/choongang/game/controllers/PokemonGameController.java
@@ -2,9 +2,11 @@ package org.choongang.game.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.choongang.global.config.annotations.Controller;
-import org.choongang.global.config.annotations.GetMapping;
-import org.choongang.global.config.annotations.RequestMapping;
+import org.choongang.game.constants.GameResult;
+import org.choongang.game.services.PokemonGameService;
+import org.choongang.global.config.annotations.*;
+import org.choongang.pokemon.entities.PokemonDetail;
+import org.choongang.pokemon.exceptions.PokemonNotFoundException;
 import org.choongang.pokemon.services.PokemonInfoService;
 
 import java.util.List;
@@ -14,6 +16,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PokemonGameController {
 
+    private final PokemonGameService gameService;
+    private final PokemonInfoService infoService;
     private final HttpServletRequest request;
 
     @GetMapping
@@ -35,9 +39,19 @@ public class PokemonGameController {
         return "/game/step1";
     }
 
-    @GetMapping("/step2")
-    public String step2() {
+    @PostMapping("/step2")
+    public String step2(@RequestParam("seq") long seq) {
         commonProcess();
+        PokemonDetail user = infoService.get(seq).orElseThrow(PokemonNotFoundException::new);
+        PokemonDetail computer = infoService.getRandom().orElseThrow(PokemonNotFoundException::new);
+
+
+        GameResult result = gameService.play(user, computer);
+
+        request.setAttribute("user", user);
+        request.setAttribute("computer", computer);
+        request.setAttribute("result", result);
+
         return "/game/step2";
     }
 
