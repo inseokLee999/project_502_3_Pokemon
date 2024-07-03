1ee61c7608b2cebd71a14d69f9f4d07294a131c2
yoojung275 <yoojung275@gmail.com>
2024-07-03 19:58:11 +0900

    관리자 게시판 - 회원 관리 기능 구현(css 필요)

--- src/main/java/org/choongang/member/controllers/JoinRequest.java
+++ src/main/java/org/choongang/member/controllers/JoinRequest.java
@@ -1,7 +1,8 @@
 package org.choongang.member.controllers;
 
+import lombok.Builder;
 import lombok.Data;
-
+@Builder
 @Data
 public class JoinRequest {
     private String email;
--- src/main/java/org/choongang/member/validators/JoinValidator.java
+++ src/main/java/org/choongang/member/validators/JoinValidator.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.choongang.global.config.annotations.Component;
 import org.choongang.global.exceptions.AlertException;
 import org.choongang.member.controllers.JoinRequest;
+import org.choongang.member.exceptions.DuplicatedMemberException;
 import org.choongang.member.mappers.MemberMapper;
 
 @Component
@@ -34,7 +35,7 @@ public class JoinValidator implements Validator<JoinRequest>, RequiredValidator,
         checkTrue(checkEmail(email), new AlertException("이메일 형식이 아닙니다.", status));
 
         // 이메일 중복 여부 체크 - 이미 가입된 회원인지 여부
-        checkTrue(mapper.exists(email) == 0L, new AlertException("이미 가입된 이메일입니다.", status));
+        checkTrue(mapper.exists(email) == 0L, new DuplicatedMemberException());
 
         // 비밀번호 자리수는 8자리 이상
         checkTrue(password.length() >= 8, new AlertException("비밀번호는 8자리 이상 입력하세요.", status));
