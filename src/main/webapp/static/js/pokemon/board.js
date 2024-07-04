e152ee7c7b0a4441b90ca0bea8dce8d61979c07f
inseokLee999 <syt06137@naver.com>
2024-07-04 12:46:19 +0900

    게시판 메뉴 hover 시 흰색, 포켓몬 도감 움직이는거 계산값 튀는 현상 수정

--- src/main/webapp/static/js/pokemon/board.js
+++ src/main/webapp/static/js/pokemon/board.js
@@ -1,16 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     var containers = document.querySelectorAll('.pokemon-data');
+
     containers.forEach(function(container) {
         container.addEventListener('mousemove', function(e) {
-            let x = e.offsetX;
-            var y = e.offsetY;
-            var rotateX = 4/30 * y-20;
-            var rotateY = -1/5 * x+20;
+            let rect = container.getBoundingClientRect();
+            let x = e.clientX - rect.left;
+            let y = e.clientY - rect.top;
+            let centerX = rect.width / 2;
+            let centerY = rect.height / 2;
+            let rotateX = (centerY - y) / centerY * 20; // 중앙을 기준으로 회전 각도 계산
+            let rotateY = (x - centerX) / centerX * 20;
+
             container.style.transform = `perspective(350px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
         });
 
         container.addEventListener('mouseleave', function() {
-            container.style.transform = 'rotateY(0deg)';
+            container.style.transform = 'perspective(350px) rotateX(0deg) rotateY(0deg)';
         });
-    })
+    });
 });
